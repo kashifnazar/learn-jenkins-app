2a31df1d5f8b6406b43004b3e1d628100b5c36f2
Kashif Nazar <muhammad.kashif.nazar@gmail.com>
2025-06-01 12:03:11 +0100

    title change

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy
+          Learning Jenkins on Udemy
         </a>
       </header>
       <p>
-          Application version: 1
+        Application version: 1
       </p>
     </div>
   );
